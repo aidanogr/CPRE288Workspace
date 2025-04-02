cbf1af21b2b3c70a789b29d05a2f32c887a16e9a
Dalaliants <andreyd@iastate.edu>
2025-04-01 20:15:03 -0500

    auto commit

---
+++ Lab9/Scan.c
@@ -0,0 +1,189 @@
+/*
+ * Scan.c
+ *
+ *  Created on: Mar 11, 2025
+ *      Author: andreyd
+ */
+
+#include "Scan.h"
+
+#define SIG_DIST 850
+#define SIG_WIDTH 3
+#define SIG_DELTA
+
+
+
+
+
+void scan_range(int min_angle, int max_angle, obj_t object_array[], int *arr_size) {
+    cyBOT_Scan_t *scan_data = (cyBOT_Scan_t *) calloc(1, sizeof(cyBOT_Scan_t));
+    cyBOT_Scan(0, scan_data);
+    sleep_millis(2000);
+
+
+    int sum;
+    int i;
+    int num_objects = 0;
+    char isDetecting = 0;
+
+
+    uart_sendStr("\n\r\n\r");
+
+    while(min_angle < max_angle) {
+            sum = 0;
+            for(i = 0; i < 3; i++) {
+                cyBOT_Scan(min_angle, scan_data);
+                sum += scan_data->IR_raw_val;
+            }
+
+            if(!isDetecting) {
+                if(sum/3 > SIG_DIST) {
+                    isDetecting = 1;
+                    object_array[num_objects].start_angle = min_angle;
+                }
+
+            } else {
+                if(sum/3 < SIG_DIST) {
+                    isDetecting = 0;
+                    object_array[num_objects].end_angle = min_angle;
+                    num_objects++;
+                }
+            }
+
+
+            char msg[100];
+            sprintf(msg, "%d,%d\r\n", min_angle, sum/3);
+            uart_sendStr(msg);
+            min_angle+=2;
+        }
+
+//        if(isDetecting) {
+//            object_array[num_objects].end_angle = min_angle;
+//            num_objects++;
+//        }
+
+        *arr_size = num_objects;
+
+
+
+        for(i = 0; i < num_objects; i++) {
+            int j;
+            int avg = (object_array[i].start_angle + object_array[i].end_angle)/2;
+
+            for(j = 0; j < 10; j++) {
+                cyBOT_Scan(avg - j, NULL);
+                sleep_millis(1000);
+                cyBOT_Scan(avg - j, scan_data);
+
+                if(scan_data->sound_dist < 310 || j == 9) {
+                    object_array[i].distance = scan_data->sound_dist;
+                    object_array[i].width = 2 * object_array[i].distance * tan( ((M_PI)/360) * (object_array[i].end_angle - object_array[i].start_angle));
+                    break;
+                }
+
+
+            }
+        }
+
+        uart_sendStr("\n\r\n\r");
+        print_objects(object_array, num_objects);
+
+        free(scan_data);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//void scan_range(int min_angle, int max_angle, obj_t object_array[], int *arr_size) {
+//    cyBOT_Scan_t *scan_data = (cyBOT_Scan_t *) calloc(1, sizeof(cyBOT_Scan_t));
+//    cyBOT_Scan(0, scan_data);
+//    sleep_millis(2000);
+//
+//
+//    int sum;
+//    int i;
+//    int num_objects = 0;
+//    char isDetecting = 0;
+//
+//    uart_sendStr("\n\r\n\r");
+//
+//    while(min_angle < max_angle) {
+//            sum = 0;
+//            for(i = 0; i < 3; i++) {
+//                cyBOT_Scan(min_angle, scan_data);
+//                sum += scan_data->IR_raw_val;
+//            }
+//
+//            if(!isDetecting) {
+//                if(sum/3 > SIG_DIST) {
+//                    isDetecting = 1;
+//                    object_array[num_objects].start_angle = min_angle;
+//                }
+//
+//            }
+//            else {
+//                if(sum/3 < SIG_DIST) {
+//                    isDetecting = 0;
+//                    object_array[num_objects].end_angle = min_angle;
+//                    num_objects++;
+//                }
+//            }
+//
+//
+//            char msg[100];
+//            sprintf(msg, "%d,%d\r\n", min_angle, sum/3);
+//            uart_sendStr(msg);
+//            min_angle+=2;
+//        }
+//
+//        if(isDetecting) {
+//            object_array[num_objects].end_angle = min_angle;
+//            num_objects++;
+//        }
+//
+//        *arr_size = num_objects;
+//
+//
+//
+//        for(i = 0; i < num_objects; i++) {
+//            int avg = (object_array[i].start_angle + object_array[i].end_angle)/2;
+//            cyBOT_Scan(avg, scan_data);
+//            sleep_millis(600);
+//            cyBOT_Scan(avg, scan_data);
+//            object_array[i].distance = scan_data->sound_dist;
+//            object_array[i].width = 2 * object_array[i].distance * tan( ((M_PI)/360) * (object_array[i].end_angle - object_array[i].start_angle));
+//        }
+//
+//        uart_sendStr("\n\r\n\r");
+//        print_objects(object_array, *arr_size);
+//}
+
+void print_objects(obj_t arr[], int size) {
+
+    int i;
+    for(i = 0; i < size; i++) {
+        char msg[100];
+        sprintf(msg, "%d,%d, %lf, %lf\r\n", arr[i].start_angle, arr[i].end_angle, arr[i].distance, arr[i].width);
+        uart_sendStr(msg);
+    }
+
+}
+
---
+++ Lab9/main.c
@@ -0,0 +1,53 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 30, 2025
+ *      Author: ahogrady
+ *
+ *      LAST COMMIT: FIXED INITIAL DISTANCE IN "move_forward(...)"
+ *                   ADDED "move_forward1(...)" FOR EASE-IN-OUT ALGORITHM
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include "open_interface.h"
+#include "movement.h"
+#include "cyBot_uart.h"
+#include "cyBot_Scan.h"
+#include "uart-interrupt.h"
+#include "Scan.h"
+#include "sleep.h"
+#include "adc.h"
+#include "ping.h"
+
+
+
+
+
+
+int main() {
+    oi_t *sensor_data = oi_alloc(); // do this only once at start of main()
+    //oi_init(sensor_data); // do this only once at start of main()
+
+    timer_init(); // Initialize Timer, needed before any LCD screen functions can be called
+                      // and enables time functions (e.g. timer_waitMillis)
+
+    lcd_init();   // Initialize the LCD screen.  This also clears the screen.
+    uart_interrupt_init();
+    cyBOT_init_Scan(0b111);
+    ping_init();
+
+    //cyBOT_SERVO_cal();
+    right_calibration_value = 238000;
+    left_calibration_value = 1230250;
+
+    while(1) {
+        timer_waitMillis(1);
+        ping_trigger();
+
+    }
+
+}
+
+
+
